7f29f70c1a470d14ec170df00a50da6c7abd1bf7
Beholi <benenge1997@gmail.com>
2021-03-04 00:36:16 -0500

    modified App.js to say {insert your name here}

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './Beholi.jpeg';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          My name is Beholi and i'm going to master React!
+        </p>
+        <a
+          className="App-link"
+          href= "https://github.com/Benenge97"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Check Me Out!
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
